088066563c6c1c4689d1b356615621cf6e82eede
baligh <baligh@77gamil.com>
2025-09-26 04:34:21 +0300

    2

---
+++ mybaligh/lib/main.dart
@@ -0,0 +1,11 @@
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
+import 'package:crypto/crypto.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:screen_protector/screen_protector.dart';
